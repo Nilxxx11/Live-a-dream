e5e4e514f67937edaf8be88b42061ed17aa71cf2
Nilxxx11 <80600547+Nilxxx11@users.noreply.github.com>
2025-04-05 17:21:00 -0500

    update script.js

--- script.js
+++ script.js
@@ -24,6 +24,16 @@ function renderPaseos() {
     section.className = 'paseo';
     section.innerHTML = `
       <img src="${paseo.imagen}" alt="${paseo.titulo}">
+      <section class="video-section">
+  <h2>Descubre la experiencia</h2>
+  <iframe width="100%" height="400" style="max-width: 800px; border-radius: 10px;"
+    src="https://www.youtube.com/embed/VIDEO_ID"
+    title="Video de presentación"
+    frameborder="0"
+    allowfullscreen>
+  </iframe>
+</section>
+
       <h2>${paseo.titulo}</h2>
       <p>${paseo.descripcionCorta}</p>
       <button onclick="abrirModal('${paseo.titulo}', '${paseo.descripcionLarga}', '${paseo.imagen}', '${paseo.precio}')">Ver más</button>
